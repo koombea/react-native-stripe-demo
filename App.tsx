5656156411744198d8d56625414ba602dd563b42
dionart <arthuroliveira@eng.ci.ufpb.br>
2022-09-16 13:24:56 -0300

    add: adding stripe card input and api call to home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import { StripeProvider } from '@stripe/stripe-react-native';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import HomeScreen from './src/screens/HomeScreen';
 
+const queryClient = new QueryClient();
+
 const App = () => {
+  const [publishableKeyForStripe, setPublishableKeyForStripe] = React.useState<
+    string | null
+  >();
+
+  useEffect(() => {
+    const getPublishableKey = async () => {
+      const res = await fetch('http://localhost:3000/publishableKey', {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const { publishableKey } = await res.json();
+      setPublishableKeyForStripe(publishableKey);
+    };
+    getPublishableKey();
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <HomeScreen />
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <StripeProvider
+        publishableKey={publishableKeyForStripe || ''}
+        urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+      >
+        <SafeAreaView style={styles.container}>
+          <HomeScreen />
+        </SafeAreaView>
+      </StripeProvider>
+    </QueryClientProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: '100%',
+    height: '100%',
   },
 });
 
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,48 @@
+/* eslint-disable react-native/no-inline-styles */
+import { CardField } from '@stripe/stripe-react-native';
+import { Details } from '@stripe/stripe-react-native/lib/typescript/src/types/components/CardFieldInput';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const HomeScreen: React.FC = () => {
+  const addCardToStripe = async (cardDetails: Details) => {
+    const res = await fetch('http://localhost:3000/create-payment-card', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        number: cardDetails.number,
+        exp_month: cardDetails.expiryMonth,
+        exp_year: cardDetails.expiryYear,
+        cvc: cardDetails.cvc,
+      }),
+    });
+
+    const test = await res.json();
+    console.log(test);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>HomePage</Text>
+      <CardField
+        dangerouslyGetFullCardDetails
+        postalCodeEnabled={false}
+        placeholders={{
+          number: '4242 4242 4242 4242',
+        }}
+        cardStyle={{
+          backgroundColor: '#FFFFFF',
+          textColor: '#000000',
+        }}
+        style={{
+          width: '100%',
+          height: 50,
+          marginVertical: 30,
+        }}
+        onCardChange={cardDetails => {
+          cardDetails.complete && addCardToStripe(cardDetails);
+          console.log('cardDetails', cardDetails);
+        }}
+      />
     </View>
   );
 };
@@ -13,6 +51,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#ffff',
     alignItems: 'center',
   },
 });
